1dc10ab6cf3c791872486729797cbe4c804d1add
matheus mozart <mmsnborges@gmail.com>
2024-08-30 10:31:49 -0300

    about

--- front-end/src/pages/home/index.tsx
+++ front-end/src/pages/home/index.tsx
@@ -16,6 +16,23 @@ export function HomeScreen() {
           <Button text={'Saiba mais'} redirectTo="#" onClick={undefined} />
         </div>
       </div>
+
+      <div className={styles.about__container}>
+        <div className={styles.about__container__content}>
+          <div className={styles.about__container__content__text}>
+            <h2>Algo importante</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni blanditiis quas optio
+              ad accusamus, cupiditate itaque ex, dignissimos vero sapiente suscipit magnam incidunt
+              accusantium libero quia nam inventore iste excepturi.
+            </p>
+            <Button text={'Saiba mais'} />
+          </div>
+        </div>
+        <div className={styles.about__container__content}>
+          <img src="/sobre.png" alt="sobre_nos_tasks" />
+        </div>
+      </div>
     </>
   );
 }
